e62722a6eb871946d2d285c23119e9edae9b1b03
Roberto Ramirez Sanchez <aleks9506@Mac-mini-de-Roberto.local>
2015-08-04 23:12:56 -0500

    Initial Commit

---
+++ testtappsi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testtappsi
+//
+//  Created by Roberto Ramirez Sanchez on 04/08/15.
+//  Copyright (c) 2015 Asistelec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testtappsi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testtappsi
+//
+//  Created by Roberto Ramirez Sanchez on 04/08/15.
+//  Copyright (c) 2015 Asistelec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
